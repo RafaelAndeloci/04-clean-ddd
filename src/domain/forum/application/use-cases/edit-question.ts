dc9fc269ada9cb3f4dc0a2e1764e2cc63e9ca349
Rafael Andeloci <rafaelandeloci.dev@gmail.com>
2025-02-24 20:38:49 -0300

    184: uaau

--- src/domain/forum/application/use-cases/edit-question.ts
+++ src/domain/forum/application/use-cases/edit-question.ts
@@ -1,5 +1,9 @@
 import { Either, left, right } from "@/core/either";
+import { UniqueEntityId } from "@/core/entities/unique-entity-id";
 import { Question } from "../../enterprise/entities/question";
+import { QuestionAttachment } from "../../enterprise/entities/question-attachment";
+import { QuestionAttachmentList } from "../../enterprise/entities/question-attachment-list";
+import { QuestionAttachmentsRepository } from "../repositories/question-attachments-repository";
 import { QuestionsRepository } from "../repositories/questions-repository";
 import { NotAllowedError } from "./errors/not-allowed.error";
 import { ResourceNotFoundError } from "./errors/resource-not-found.error";
@@ -9,6 +13,7 @@ export interface EditQuestionUseCaseRequest {
   questionId: string;
   title: string;
   content: string;
+  attachmentIds: string[];
 }
 
 type EditQuestionUseCaseResponse = Either<
@@ -17,12 +22,16 @@ type EditQuestionUseCaseResponse = Either<
 >;
 
 export class EditQuestionUseCase {
-  constructor(private questionsRepository: QuestionsRepository) {}
+  constructor(
+    private questionsRepository: QuestionsRepository,
+    private questionAttachmentRepository: QuestionAttachmentsRepository
+  ) {}
   async execute({
     questionId,
     content,
     title,
     authorId,
+    attachmentIds,
   }: EditQuestionUseCaseRequest): Promise<EditQuestionUseCaseResponse> {
     const question = await this.questionsRepository.findById(questionId);
 
@@ -34,8 +43,25 @@ export class EditQuestionUseCase {
       return left(new NotAllowedError());
     }
 
+    const currentQuestionAttachments =
+      await this.questionAttachmentRepository.findManyByQuestionId(questionId);
+
+    const questionAttachmentList = new QuestionAttachmentList(
+      currentQuestionAttachments
+    );
+
+    const questionAttachments = attachmentIds.map((attachmentId) => {
+      return QuestionAttachment.create({
+        attachmentId: new UniqueEntityId(attachmentId),
+        questionId: question.id,
+      });
+    });
+
+    questionAttachmentList.update(questionAttachments);
+
     question.title = title;
     question.content = content;
+    question.attachments = questionAttachmentList;
 
     await this.questionsRepository.save(question);
 
--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -3,12 +3,12 @@ import { UniqueEntityId } from "@/core/entities/unique-entity-id";
 import { Optional } from "@/core/types/optional";
 import { Slug } from "@/domain/forum/enterprise/entities/value-objects/slug";
 import dayjs from "dayjs";
-import { QuestionAttachment } from "./question-attachment";
+import { QuestionAttachmentList } from "./question-attachment-list";
 
 export interface QuestionProps {
   authorId: UniqueEntityId;
   bestAnswerId?: UniqueEntityId;
-  attachments: QuestionAttachment[];
+  attachments: QuestionAttachmentList;
   title: string;
   content: string;
   slug: Slug;
@@ -32,7 +32,7 @@ export class Question extends AggregateRoot<QuestionProps> {
   get attachments() {
     return this.props.attachments;
   }
-  set attachments(attachments: QuestionAttachment[]) {
+  set attachments(attachments: QuestionAttachmentList) {
     this.props.attachments = attachments;
   }
 
@@ -82,7 +82,7 @@ export class Question extends AggregateRoot<QuestionProps> {
         ...props,
         slug: props.slug ?? Slug.createFromText(props.title),
         createdAt: props.createdAt ?? new Date(),
-        attachments: props.attachments ?? [],
+        attachments: props.attachments ?? new QuestionAttachmentList(),
       },
       id
     );
