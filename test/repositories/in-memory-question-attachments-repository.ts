7afcd56534379a9c72f46248b7339e2aa3454775
Rafael Andeloci <rafael.goncalves@bkbank.com.br>
2025-06-02 14:50:35 -0300

    chore: deleting question attachments when deleting questions

--- test/repositories/in-memory-question-attachments-repository.ts
+++ test/repositories/in-memory-question-attachments-repository.ts
@@ -4,6 +4,13 @@ import { QuestionAttachment } from "@/domain/forum/enterprise/entities/question-
 export class InMemoryQuestionAttachmentsRepository
   implements QuestionAttachmentsRepository
 {
+  async deleteManyByQuestionId(questionId: string): Promise<void> {
+    const questionAttachments = this.items.filter((item) => {
+      item.questionId.toString() !== questionId;
+    });
+
+    this.items = questionAttachments;
+  }
   public items: QuestionAttachment[] = [];
 
   async findManyByQuestionId(questionId: string) {
--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -1,8 +1,13 @@
 import { PaginationParams } from "@/core/repositories/pagination-params";
+import { QuestionAttachmentsRepository } from "@/domain/forum/application/repositories/question-attachments-repository";
 import { QuestionsRepository } from "@/domain/forum/application/repositories/questions-repository";
 import { Question } from "@/domain/forum/enterprise/entities/question";
 
 export class InMemoryQuestionsRepository implements QuestionsRepository {
+  constructor(
+    private questionAttachmentsRepository: QuestionAttachmentsRepository
+  ) {}
+
   async findById(id: string): Promise<Question | null> {
     const question = this.items.find((item) => item.id.toString() === id);
     return question ?? null;
@@ -36,5 +41,9 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
   async delete(question: Question): Promise<void> {
     const itemIndex = this.items.findIndex((item) => item.id === question.id);
     this.items.splice(itemIndex, 1);
+
+    this.questionAttachmentsRepository.deleteManyByQuestionId(
+      question.id.toString()
+    );
   }
 }
